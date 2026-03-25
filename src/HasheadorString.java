ef2f3674aaaabdf3b7861bce2d83bc21bdf3773b
Arthur Honorio <hno.arthur@gmail.com>
2026-03-25 20:13:58 -0300

    fix: utiliza exatamente o mesmo sha1 do link fornecido

--- src/HasheadorString.java
+++ src/HasheadorString.java
@@ -1,23 +1,20 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class HasheadorString{
-    public static String sha1(String entrada){
-        try{
-            MessageDigest algoritmo = MessageDigest.getInstance("SHA-1");
-
-            byte[] bytesE = entrada.getBytes("UTF-8");
-            byte[] bytesH = algoritmo.digest(bytesE);
-
-            StringBuilder hexadecimal = new StringBuilder();
-
-            for (byte b : bytesH){
-                hexadecimal.append(String.format("%02x", 0xFF & b));
+public class HasheadorString {
+    public static String sha1(String entrada) {
+        try {
+            MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+            byte[] result = mDigest.digest(entrada.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < result.length; i++) {
+                sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
             }
-            return hexadecimal.toString();
-        } catch(NoSuchAlgorithmException e){
+
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("Algoritimo SHA-1 não encontrado: " + e.getMessage());
-        } catch(Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("Erro ao gerar hash: " + e.getMessage());
         }
     }
